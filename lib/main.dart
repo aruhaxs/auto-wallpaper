752e327bed2e4539ec07f00bdb3f9e7f5d2fbb83
dwirez99 <mrlhafizh@gmail.com>
2025-08-10 11:16:13 +0700

    feat: add multi-image selection and cropping

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart'; // Impor Google Fonts
+import 'package:hive_flutter/hive_flutter.dart';
 import 'features/wallpaper/ui/home_page.dart';
 
-void main() {
+const Color primaryColor = Color(0xFF8A8AFF);
+const Color scaffoldBackgroundColor = Color(0xFFF8F7FF);
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
+  await Hive.openBox('wallpaper_settings');
   runApp(const MyApp());
 }
 
@@ -14,8 +22,33 @@ class MyApp extends StatelessWidget {
       title: 'Auto Wallpaper',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
         useMaterial3: true,
+        scaffoldBackgroundColor: scaffoldBackgroundColor,
+        colorScheme: ColorScheme.fromSeed(seedColor: primaryColor),
+
+        textTheme: GoogleFonts.poppinsTextTheme(Theme.of(context).textTheme),
+
+        appBarTheme: const AppBarTheme(
+          backgroundColor: primaryColor,
+          foregroundColor: Colors.white,
+          elevation: 2.0,
+          centerTitle: true,
+        ),
+
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: primaryColor,
+            foregroundColor: Colors.white,
+            padding: const EdgeInsets.symmetric(vertical: 14, horizontal: 24),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(12),
+            ),
+            textStyle: const TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
       ),
       home: const HomePage(),
     );
